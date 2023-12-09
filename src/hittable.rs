a98711a3a3d5fe7c7c9a4446252ed1b9c9269335
Peng Lei <swk-pl@163.com>
2023-12-09 14:03:50 +0800

    feat(hittable): add hittable list which contains shpaes

--- src/hittable.rs
+++ src/hittable.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 use crate::{
     ray::Ray,
     vec3::{Point, Vec3},
@@ -25,4 +27,22 @@ impl HitRecord {
 
 pub trait Hittable {
     fn hit(&self, ray: &Ray, ray_tmin: f64, ray_tmax: f64) -> Option<HitRecord>;
+}
+
+pub type HittableList = Vec<Rc<dyn Hittable>>;
+
+impl Hittable for HittableList {
+    fn hit(&self, ray: &Ray, ray_tmin: f64, ray_tmax: f64) -> Option<HitRecord> {
+        let mut closest_so_far = ray_tmax;
+        let mut hit_record = None;
+
+        for shape in self {
+            if let Some(hit_something) = shape.hit(ray, ray_tmin, closest_so_far) {
+                closest_so_far = hit_something.t;
+                hit_record = Some(hit_something);
+            }
+        }
+
+        return hit_record;
+    }
 }
