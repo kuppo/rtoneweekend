487e853cce9ed21b39a9ac5aaa0a8d18c5fb76f2
Peng Lei <swk-pl@163.com>
2023-12-10 13:57:29 +0800

    final result. Yeah, collision would happen.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
+use rand::Rng;
 use rtoneweekend::{
     camera::{Camera, CameraConfig},
-    hittable::Hittable,
+    hittable::HittableList,
     material::{Dieletric, Lambertian, Metal, Rgb},
     shape::Sphere,
     vec3::{Point, Vec3},
@@ -8,55 +9,118 @@ use rtoneweekend::{
 use std::rc::Rc;
 
 fn main() {
-    let mut random_generator = rand::thread_rng();
-    let mut camera = Camera::create(CameraConfig {
-        width: 800,
-        max_depth: 10,
-        samples_per_pixel: 20,
+    let mut camera: Camera = Camera::create(CameraConfig {
+        width: 2560,
         vfov: 20.0,
-        look_from: Point::new(-2.0, 2.0, 1.0),
-        look_at: Point::new(0.0, 0.0, -1.0),
+        max_depth: 20,
+        samples_per_pixel: 100,
+        look_from: Point::new(13.0, 2.0, 3.0),
+        look_at: Point::new(0.0, 0.0, 0.0),
         camera_vup: Vec3::new(0.0, 1.0, 0.0),
-        defocus_angle: 10.0,
-        focus_dist: 3.4,
+        float_correction: 0.00001,
+        defocus_angle: 0.6,
+        focus_dist: 10.0,
+        save_path: "/tmp/pic.png",
+        aspect_ratio: 16.0 / 9.0,
         ..Default::default()
     });
+    // dbg!(&camera);
+
+    let mut random_generator = rand::thread_rng();
 
     // world
-    let world: Vec<Rc<dyn Hittable>> = vec![
-        Rc::new(Sphere {
-            center: Point::new(0.0, -100.5, -1.0),
-            radius: 100.0,
-            material: Rc::new(Lambertian {
-                albedo: Rgb::new(0.8, 0.8, 0.0),
-            }),
-        }),
-        Rc::new(Sphere {
-            center: Point::new(0.0, 0.0, -1.0),
-            radius: 0.5,
-            material: Rc::new(Lambertian {
-                albedo: Rgb::new(0.1, 0.2, 0.5),
-            }),
-        }),
-        Rc::new(Sphere {
-            center: Point::new(-1.0, 0.0, -1.0),
-            radius: 0.5,
-            material: Rc::new(Dieletric { ir: 1.5 }),
-        }),
-        Rc::new(Sphere {
-            center: Point::new(-1.0, 0.0, -1.0),
-            radius: -0.4,
-            material: Rc::new(Dieletric { ir: 1.5 }),
-        }),
-        Rc::new(Sphere {
-            center: Point::new(1.0, 0.0, -1.0),
-            radius: 0.5,
-            material: Rc::new(Metal {
-                albedo: Rgb::new(0.8, 0.6, 0.2),
-                fuzz: 0.0,
-            }),
-        }),
-    ];
+    let mut world: HittableList = HittableList::new();
+    let ground_material = Rc::new(Lambertian {
+        albedo: Rgb::new(0.5, 0.5, 0.5),
+    });
+    let ground = Rc::new(Sphere {
+        center: Point::new(0.0, -1000.0, 0.0),
+        radius: 1000.0,
+        material: ground_material.clone(),
+    });
+    world.push(ground.clone());
+
+    for a in -11..11 {
+        for b in -11..11 {
+            let choose_mat = random_generator.gen_range(0.0..1.0);
+            let center = Point::new(
+                a as f64 + 0.9 * random_generator.gen_range(0.0..1.0),
+                0.2,
+                b as f64 + 0.9 * random_generator.gen_range(0.0..1.0),
+            );
+
+            if (center - Point::new(4.0, 0.2, 0.0)).length() > 0.9 {
+                match choose_mat {
+                    a if a < 0.8 => {
+                        let albedo = Rgb::new(
+                            random_generator.gen_range(0.0..1.0),
+                            random_generator.gen_range(0.0..1.0),
+                            random_generator.gen_range(0.0..1.0),
+                        ) * Rgb::new(
+                            random_generator.gen_range(0.0..1.0),
+                            random_generator.gen_range(0.0..1.0),
+                            random_generator.gen_range(0.0..1.0),
+                        );
+                        let sphere_material = Rc::new(Lambertian { albedo });
+                        let rand_sphere = Rc::new(Sphere {
+                            center,
+                            radius: 0.2,
+                            material: sphere_material.clone(),
+                        });
+                        world.push(rand_sphere.clone());
+                    }
+                    b if b < 0.95 => {
+                        let albedo = Rgb::new(
+                            random_generator.gen_range(0.5..1.0),
+                            random_generator.gen_range(0.5..1.0),
+                            random_generator.gen_range(0.5..1.0),
+                        );
+                        let fuzz = random_generator.gen_range(0.0..0.5);
+                        let sphere_material = Rc::new(Metal { albedo, fuzz });
+                        let rand_sphere = Rc::new(Sphere {
+                            center,
+                            radius: 0.2,
+                            material: sphere_material.clone(),
+                        });
+                        world.push(rand_sphere.clone());
+                    }
+                    _ => {
+                        let sphere_meterial = Rc::new(Dieletric { ir: 1.5 });
+                        let rand_sphere = Rc::new(Sphere {
+                            center,
+                            radius: 0.2,
+                            material: sphere_meterial.clone(),
+                        });
+                        world.push(rand_sphere.clone());
+                    }
+                }
+            }
+        }
+    }
+
+    let material1 = Rc::new(Dieletric { ir: 1.5 });
+    world.push(Rc::new(Sphere {
+        center: Point::new(0.0, 1.0, 0.0),
+        radius: 1.0,
+        material: material1.clone(),
+    }));
+    let material2 = Rc::new(Lambertian {
+        albedo: Rgb::new(0.4, 0.2, 0.1),
+    });
+    world.push(Rc::new(Sphere {
+        center: Point::new(-5.0, 1.0, 0.0),
+        radius: 1.0,
+        material: material2.clone(),
+    }));
+    let material3 = Rc::new(Metal {
+        albedo: Rgb::new(0.7, 0.6, 0.5),
+        fuzz: 0.0,
+    });
+    world.push(Rc::new(Sphere {
+        center: Point::new(4.0, 1.0, 0.0),
+        radius: 1.0,
+        material: material3.clone(),
+    }));
 
     camera.render(&world, &mut random_generator);
 }
